0fab0b743ff152e351a3fdee9e277a350c9901fa
zecchi98 <78590047+zecchi98@users.noreply.github.com>
2022-03-16 11:57:26 +0100

    Add files via upload

---
+++ scripts/UI_gara.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5 import uic, QtWidgets,QtGui
+import rospy
+from std_msgs.msg import String
+from ur10_control.srv import UserInterface,UserInterfaceRequest,aruco_service,aruco_serviceRequest
+from geometry_msgs.msg import Pose
+from tf import transformations
+import rospkg
+import time
+import getch
+
+def menu():
+    choice=-1
+    while(choice!='0'):
+        print("select the action:")
+        print("0) shut down")
+        print("1) base position")
+        print("2) change target aruco")
+        print("3) reach target")
+        print("4) gripper")
+        print("5) joystick")
+        print("6) ruota e cerca aruco")
+        print("7) Stampa pose")
+        print("8) Freno a mano")
+        print("9) Salva aruco visibili")
+        print("10) Esplora")
+        print("11) TXT_SAVE_ARUCO")
+        print("12) superdebug")
+        print("13) centra aruco")
+        print("14)ON/OFF Camera window")
+        choice=input("choice: ")
+
+        if(choice=='0'):
+            modality='exit'
+            target_pose=Pose()
+            target_joints=[0,0,0,0,0,0]
+            second_information='null'
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+
+        elif(choice=='1'):
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='automazione_go_pos_iniziale'
+            second_information=''
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+
+        elif(choice=='2'):
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='automazione_pannello_nextAruco'
+            second_information=str(input("target aruco:"))
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+
+        elif(choice=='3'):
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='automazione_pannello_MoveToSelectedAruco'
+            second_information='null'
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+
+        elif(choice=='4'):
+            print("choose gripper position: ")
+            print("1) open")
+            print("2) semi_open")
+            print("3) semi_close")
+            print("4) close")
+
+            gripper_command=input("gripper position: ")
+
+            if(gripper_command==1):
+                second_information="open"
+            elif(gripper_command==2):
+                second_information="semi_open"
+            elif(gripper_command==3):
+                second_information="semi_close"
+            elif(gripper_command==4):
+                second_information="close"
+
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='controlla_gripper'
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+
+        elif(choice=='5'):
+            input_char='0'
+            close_char='e'
+            print("press 'e' for exit from joystick")
+            print('COMMAND: ')
+            while(input_char!=close_char):
+                #input_char=input("Joystick command:")
+
+                input_char=getch.getch()
+                #print(input_char)
+                modality='joystick'
+                target_pose=Pose()
+                target_joints=[0,0,0,0,0,0]
+                second_information=input_char
+                msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+                resp1 = serv(msg)
+
+        elif(choice=='6'):
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='ruota_e_cerca_aruco'
+            second_information='null'
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+        elif(choice=='7'):
+
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='Stampa_pose_robot'
+            second_information='null'
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+        elif(choice=='8'):
+            modality='stop_trajectory'
+            second_information=''
+            msg=aruco_serviceRequest(modality,second_information)
+            resp1 = bridge_serv(msg)
+
+        elif(choice=='9'):
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='salva_aruco'
+            second_information='null'
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+        elif(choice=='10'):
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='esplora_tutto'
+            second_information='null'
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+        elif(choice=='11'):
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='save_aruco_in_txt'
+            second_information='null'
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+        elif(choice=='12'):
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='superdebug'
+            second_information='null'
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+        elif(choice=='13'):
+                target_joints=[0,0,0,0,0,0]
+                target_pose=Pose()
+                modality='centra_aruco'
+                second_information='null'
+                msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+                resp1 = serv(msg)
+        elif(choice=='14'):
+            target_joints=[0,0,0,0,0,0]
+            target_pose=Pose()
+            modality='turn_on_off_camera'
+            second_information=''
+            msg=UserInterfaceRequest(modality,second_information,target_pose,target_joints)
+            resp1 = serv(msg)
+        else:
+            print('COMMAND NOT KNOWN')
+
+        print('\n \n \n \n')
+def init():
+    print("initialization")
+    global serv,bridge_serv
+    rospy.init_node('UI_gara', anonymous=True)
+    serv = rospy.ServiceProxy('/user_interface_serv', UserInterface)
+    bridge_serv= rospy.ServiceProxy('/aruco_modality', aruco_service)
+if __name__ == "__main__":
+    init()
+    menu()
+
---
+++ scripts/my_opencv.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+from tkinter.tix import Tree
+import rospy
+import numpy as np
+
+import sys
+import os
+from sensor_msgs.msg import Image as sensImg
+from sensor_msgs.msg import CameraInfo
+from std_srvs.srv import Empty
+import cv2 as cv
+import cv2.aruco as aruco
+from cv_bridge import CvBridge
+from ur10_control.srv import *
+def handler_cv_server(msg):
+    global bool_exit,bool_camera_on
+    if msg.message=="exit":
+        bool_exit=True
+    if msg.message=="turn_on_off_camera":
+        bool_camera_on=not (bool_camera_on)
+    return cv_serverResponse()
+
+def loadCameraParam(myCam):
+    ##
+    #\brief this function will load camera parameters
+    #@param myCam Topic of the camera
+    #@return No return
+    global cameraMatr,cameraDistCoefs,cameraFocLen
+    global cameraDistCoefs
+    global cameraFocLen
+    
+    print('loading camera parameters...')
+    cameraInfoMsg=rospy.wait_for_message(myCam+'/camera_info',CameraInfo)
+    cameraMatr=np.reshape(cameraInfoMsg.K,[3,3])
+    cameraDistCoefs=cameraInfoMsg.D
+    cameraFocLen=np.mean([np.ravel(cameraMatr[0])[0],np.ravel(cameraMatr[1])[1]])
+def callbackRaw(raw_img):
+    ##
+    #\brief This function is the callback which is subscribed to the raw channel of the image. It will detect arucos and in case it is a new one it will save it and check for victory.
+    #@param raw_img It is a sensor_msgs of type Image which contain the image we will analyze
+    #@return No return
+
+    global bool_exit
+
+    #if this bool is a true than it close everything
+    if bool_exit:
+        cv.destroyAllWindows()
+        os._exit(os.EX_OK)
+
+    #Here the detection will be computed based on the dictionary
+    cv_image=bridge.imgmsg_to_cv2(raw_img, desired_encoding='passthrough')
+    cv_gray=cv.cvtColor(cv_image,cv.COLOR_RGB2GRAY)
+    detCorners, detIds, _ = aruco.detectMarkers(cv_gray, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
+        
+    # Check if at least one marker has been found
+    if detIds is not None and len(detIds) >= 1:
+
+        #Draw the borders around the aruco
+        detAruImg = aruco.drawDetectedMarkers(cv_image.copy(), detCorners, borderColor=(0, 255, 0))
+
+        #Foreach aruco found it will work on it
+        for mId, aruPoints in zip(detIds, detCorners):
+            nulla=0
+    else:
+        detAruImg=cv_image.copy()#
+    if bool_camera_on:
+        cv.imshow('detected markers',detAruImg)
+    else:
+        cv.destroyAllWindows()
+    key = cv.waitKey(12) & 0xFF
+def main():
+    ##
+    #\brief Here we initialize some variables and thanks to spin we wait for callbackRaw
+    global bridge,bool_exit,ARUCO_PARAMETERS,ARUCO_DICT,bool_camera_on
+
+    bool_exit=False
+    bridge=CvBridge()
+
+    #Camera topics
+    raw_topic="/camera/color/image_raw"
+    myCamera="/camera/color"
+
+    bool_camera_on=True
+    
+    #rospy initialization
+    rospy.init_node('camera_listener', anonymous=True)
+    #Subscriber initialization (image callback)
+    rospy.Subscriber(raw_topic,sensImg,callbackRaw,queue_size = 1)
+    rospy.Service("/cv_server",cv_server,handler_cv_server)
+    
+    ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+    ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+
+
+    print('connecting to:'+myCamera+'...')
+    loadCameraParam(myCamera)
+    print('ready')
+
+    #Thanks to spin we will wait callbacks
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:#
+        print('Closing')
+    cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
